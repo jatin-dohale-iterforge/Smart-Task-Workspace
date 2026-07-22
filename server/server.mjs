94e1c0e2143659e19e1143e9b9b81bfd740f1962
himanshi-mishra-iterforge <himanshi.mishra@iterforge.com>
2026-07-22 20:34:42 +0530

    improve task management with role based access

--- server/server.mjs
+++ server/server.mjs
@@ -43,8 +43,6 @@ function verifyToken(req, res, next) {
 
 function authorizeRoles(...roles) {
   return (req, res, next) => {
-    console.log("User role:", req.user.role);
-    console.log("Allowed:", roles);
 
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({
